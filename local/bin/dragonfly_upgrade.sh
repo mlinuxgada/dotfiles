936e6e360de32eb35fab38107f058907a706edda
Mihail Krastev <mlinuxgada@gmail.com>
2022-01-25 14:12:12 +0200

    DragonflyBSD - change upgrade command - wip

--- local/bin/dragonfly_upgrade.sh
+++ local/bin/dragonfly_upgrade.sh
@@ -1,2 +1,3 @@
 
-cd /usr/src && make -j5 buildworld && make -j5 buildkernel && make installkernel && make installworld && make upgrade
+#cd /usr/src && make -j5 buildworld && make -j5 buildkernel && make installkernel && make installworld && make upgrade
+cd /usr/src && make -j5 build-all && make install-all && sync
